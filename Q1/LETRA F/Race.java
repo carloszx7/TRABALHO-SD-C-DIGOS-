5c736c0cfb6dde70f5a4b7bbd8b7a98c5a5495f7
José Carlos <167440479+carloszx7@users.noreply.github.com>
2025-10-16 09:25:30 -0300

    Update Race.java

--- Q1/LETRA F/Race.java
+++ Q1/LETRA F/Race.java
@@ -7,17 +7,17 @@ public class Race{
         List<Thread> evenRacers = new ArrayList<>();
 
         System.out.println("\n--- Iniciando Racers Ímpares ---\n");
-        for (int i = 1; i <= 9; i += 2) { //racers ímpares (1, 3, 5, 7, 9)
+        for (int i = 1; i <= 9; i += 2) { // Racers ímpares (1, 3, 5, 7, 9)
             RacerRunnable racer = new RacerRunnable(i);
             Thread thread = new Thread(racer);
             oddRacers.add(thread);
             thread.start();
         }
 
-        //espera que todos os racers ímpares terminem
+        // Espera que todos os racers ímpares terminem
         for (Thread thread : oddRacers) {
             try {
-                thread.join(); //espera esta thread terminar
+                thread.join(); // Espera esta thread terminar
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 System.out.println("A thread principal foi interrompida enquanto esperava pelos ímpares.");
@@ -25,14 +25,21 @@ public class Race{
         }
         System.out.println("\n--- Racers Ímpares Terminados. Iniciando Racers Pares ---\n");
 
-        for (int i = 2; i <= 10; i += 2) { //racers pares (2, 4, 6, 8, 10)
+        for (int i = 2; i <= 10; i += 2) { // Racers pares (2, 4, 6, 8, 10)
             RacerRunnable racer = new RacerRunnable(i);
             Thread thread = new Thread(racer);
             evenRacers.add(thread);
             thread.start();
         }
 
+        for (Thread thread : evenRacers) {
+            try {
+                thread.join(); // Espera esta thread terminar
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.out.println("A thread principal foi interrompida enquanto esperava pelos pares.");
+            }
+        }
         System.out.println("\n--- Todos os Racers Terminados. Fim da Corrida! ---\n");
     }
-
 }
